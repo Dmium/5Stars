8a4c7316d4cea4aabbd6f56eaec19f6806d98c18
Dom Hall <Dmium@users.noreply.github.com>
2019-07-25 17:03:34 +0000

    Initial commit

---
+++ src/install.sh
@@ -0,0 +1,4 @@
+export FLASK_APP=libraryname
+export FLASK_ENV=development
+pip3 install --user -e .
+flask run
---
+++ src/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='libraryname',
+    packages=['libraryname',
+              'libraryname.samplesubmodule'],
+    include_package_data=True,
+    install_requires=[
+    ],
+)
