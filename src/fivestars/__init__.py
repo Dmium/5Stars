7298cd7275874adf54103808ce006b5ba53dbd7c
Dom Hall <dominiczy.hall@gmail.com>
2019-07-25 19:15:52 +0100

    Wrote library

---
+++ src/fivestars/__init__.py
@@ -0,0 +1,16 @@
+import statistics
+
+def translate_user_rating(user_rating, user_ratings):
+    std = statistics.stdev(user_ratings)
+    if std == 0:
+        return 0
+    return max(-3, 
+        min(3,
+            (user_rating - statistics.mean(user_ratings)) / std
+            ))
+
+def get_normalised_rating(normalised_user_ratings):
+    return max(0,
+        min(5,
+            statistics.mean(normalised_user_ratings) + 2.5
+        ))
